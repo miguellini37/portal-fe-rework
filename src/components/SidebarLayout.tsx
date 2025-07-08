4df2d769a4fc283c9a7636baa79cc65530fc9721
cweil42 <colinweil42@gmail.com>
2025-07-07 21:45:14 -0500

    small css

--- src/components/SidebarLayout.tsx
+++ src/components/SidebarLayout.tsx
@@ -6,8 +6,8 @@ interface SidebarLayoutProps {
 
 export const SidebarLayout: React.FC<SidebarLayoutProps> = ({ children }) => {
   return (
-    <div style={{ display: 'flex', height: '100vh' }}>
-      <div style={{ width: '250px', flexShrink: 0 }}>
+    <div style={{ display: 'flex', height: '100%' }}>
+      <div style={{ width: '210px', flexShrink: 0 }}>
         <Sidebar />
       </div>
       <main style={{ flex: 1, padding: '1rem', overflowY: 'auto' }}>{children}</main>
